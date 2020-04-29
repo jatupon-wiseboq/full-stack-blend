48848c91ae41a6f1696120104a29d36d28bed22e
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-04-29 15:11:49 +0700

    Implemented: react code editor phase#1: installed module, creating the boilerplate in typescript.

--- src/public/js/construction/editor/components/CodeEditor.tsx
+++ src/public/js/construction/editor/components/CodeEditor.tsx
@@ -23,7 +23,77 @@ class CodeEditor extends Base<Props, State> {
         super(props);
         
         this.state = {
-            value: 'let x = 0;',
+            value:
+`// Auto generated code will begin and end with // Auto[*]---> and // <---Auto[*], respectively.
+// Auto[Import]--->
+import {CodeHelper} from '../helpers/CodeHelper';
+import {DeclarationHelper} from '../helpers/DeclarationHelper';
+import {IBaseProps, IBaseState, DefaultBaseProps, DefaultBaseState, Base} from './Base.js';
+// <---Auto[Import]
+
+// Import additional modules here:
+//
+import * from '...'
+
+// Auto[Declare]--->
+declare let React: any;
+declare let ReactDOM: any;
+// <---Auto[Declare]
+
+// Declare private static variables here:
+//
+let x: number = 0;
+
+// Auto[Interface]--->
+interface IProps extends IBaseProps {
+}
+interface IState extends IBaseState { 
+}
+// <---Auto[Interface]
+
+// Declare or extend interfaces here:
+//
+let DefaultProps = Object.assign({}, DefaultBaseProps, {
+  
+});
+let DefaultState = Object.assign({}, DefaultBaseState, {
+  
+});
+
+// Auto[ClassBegin]--->
+class KlassA extends Base<IProps, IState> {
+  state: IState = null;
+  protected static defaultProps: IProps = DefaultProps;
+  
+  constructor(props) {
+    super(props);
+    this.state = CodeHelper.clone(DefaultState);
+    this.initialize();
+  }
+  // <---Auto[ClassBegin]
+  
+  // Declare class variables and functions here:
+  //
+  protected initialize(): void {
+    
+  }
+
+  // Auto[ClassEnd]--->
+  protected render(): any {
+    return (
+      pug \`
+        div
+      \`
+    )
+  }
+}
+DeclarationHelper.declare('Demo.KlassA', KlassA);
+// <---Auto[ClassEnd]
+
+// Export variables here:
+//
+export {IProps, IState, DefaultProps, DefaultState, KlassA};
+`,
             enableBasicAutocompletion: true,
             enableLiveAutocompletion: true,
             fontSize: 12,
@@ -50,26 +120,87 @@ class CodeEditor extends Base<Props, State> {
         let editor = ace.edit("reactEditor");
         editor.setTheme("ace/theme/github");
         editor.session.setMode("ace/mode/typescript");
+        
+        let beginRegEx = /Auto\[[a-zA-Z]+\]--->/;
+        let endRegEx = /<---Auto\[[a-zA-Z]+\]/;
+        let lines = this.state.value.split('\n');
+        
+        editor.commands.on("exec", (function(e) {
+            if (e.command && (e.command.name == 'backspace' || e.command.name == 'insertstring')) {
+                let rowCol = editor.selection.getCursor();
+                let isPreventedFromEditing = false;
+                
+                for (let i = rowCol.row; i >= 0; i--) {
+                  if (lines[i].match(endRegEx) != null) break;
+                  if (lines[i].match(beginRegEx) != null) {
+                    isPreventedFromEditing = true;
+                    break;
+                  }
+                }
+                for (let i = rowCol.row; i < editor.session.getLength(); i++) {
+                  if (lines[i].match(beginRegEx) != null) break;
+                  if (lines[i].match(endRegEx) != null) {
+                    isPreventedFromEditing = true;
+                    break;
+                  }
+                }
+                
+                if (rowCol.column == 0 && rowCol.row-1 >= 0 && e.command.name == 'backspace') {
+                  if (lines[rowCol.row-1].match(endRegEx) != null) {
+                     isPreventedFromEditing = true;
+                  }
+                }
+                if (rowCol.row == 0) {
+                  isPreventedFromEditing = true;
+                }
+                
+                if (isPreventedFromEditing) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+            }
+        }).bind(this));
+        
+        editor.renderer.on('afterRender', function() {
+            let aceLines = [...document.getElementById('reactEditor').getElementsByClassName('ace_line')];
+            
+            for (let j=0; j<aceLines.length; j++) {
+                let readonly = false;
+              
+                for (let i = j; i >= 0; i--) {
+                    if (aceLines[i].innerText.match(endRegEx) != null) break;
+                    if (aceLines[i].innerText.match(beginRegEx) != null) {
+                        readonly = true;
+                        break;
+                    }
+                }
+                for (let i = j; i < aceLines.length; i++) {
+                    if (aceLines[i].innerText.match(beginRegEx) != null) break;
+                    if (aceLines[i].innerText.match(endRegEx) != null) {
+                        readonly = true;
+                        break;
+                    }
+                }
+                
+                if (aceLines[j]) {
+                    aceLines[j].style.opacity = (readonly) ? 0.15 : 1.0;
+                }
+            }
+        });
     }
-    onChange(newValue) {
-        console.log("change", newValue);
+    onChange(value) {
         this.setState({
-            value: newValue
+            value: value
         });
     }
   
     onSelectionChange(newValue, event) {
-        console.log("select-change", newValue);
-        console.log("select-change-event", event);
     }
   
     onCursorChange(newValue, event) {
-        console.log("cursor-change", newValue);
-        console.log("cursor-change-event", event);
     }
   
     onValidate(annotations) {
-        console.log("onValidate", annotations);
     }
     
     render() {
