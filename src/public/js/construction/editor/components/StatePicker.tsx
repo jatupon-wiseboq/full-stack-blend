86ccd6419b4bf7ec0ed55be7d54b0fa15ebe5d96
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-05-02 11:32:33 +0700

    Fixed: could not typing javascript expression in style binder.

--- src/public/js/construction/editor/components/StatePicker.tsx
+++ src/public/js/construction/editor/components/StatePicker.tsx
@@ -70,7 +70,7 @@ class StatePicker extends Base<Props, State> {
     render() {
         return (
             <div className="input-group inline state-picker" internal-fsb-event-no-propagate="click">
-                <FullStackBlend.Controls.Textbox ref="input" value={this.state.value} preRegExp="([a-zA-Z]|[a-zA-Z][a-zA-Z0-9 ]+)?" postRegExp="[a-zA-Z0-9 ]*" onUpdate={this.textboxOnUpdate.bind(this)}></FullStackBlend.Controls.Textbox>
+                <FullStackBlend.Controls.Textbox ref="input" value={this.state.value} preRegExp="[a-zA-Z0-9\.\[\]\' ]*" postRegExp="[a-zA-Z0-9\.\[\]\' ]*" onUpdate={this.textboxOnUpdate.bind(this)}></FullStackBlend.Controls.Textbox>
                     <div className="input-group-append">
                         <div className="btn btn-sm btn-secondary" internal-fsb-event-always-propagate="click">
                             <i className="fa fa-database m-0" internal-fsb-event-always-propagate="click" />
