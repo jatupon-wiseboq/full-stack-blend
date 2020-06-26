b789ddd57dd8ef5233e58662a29fa1632585aafc
Jatupon Sukkasem <jasukkas.th@gmail.com>
2020-06-26 09:56:51 +0700

    Added a hello world message.

--- src/public/js/Site.tsx
+++ src/public/js/Site.tsx
@@ -13,6 +13,7 @@ import './components/TextElement_738cbdce.js';
 import './components/TextElement_bb6cddae.js';
 import './components/TextElement_b19840c8.js';
 import './components/TextElement_92de9e1b.js';
+import './components/TextElement_0522150b.js';
 
 declare let React: any;
 declare let ReactDOM: any;
