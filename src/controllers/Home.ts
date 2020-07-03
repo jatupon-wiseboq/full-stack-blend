34a710d9a7a78dc77dba291f1e5c1e3c12414d50
Jatupon Sukkasem <jasukkas.th@gmail.com>
2020-07-03 10:38:58 +0700

    Fixed: code bugs.

--- src/controllers/Home.ts
+++ src/controllers/Home.ts
@@ -7,10 +7,10 @@ import Component74d822ea from "./components/_74d822ea.js";
 
 export const index = (req: Request, res: Response) => {
 	new Componentindex(req, res, "home/index");
-};
+}
 export const _74d822ea = (req: Request, res: Response) => {
 	new Component74d822ea(req, res, "home/_74d822ea");
-};
+}
 
 // <--- Auto[Generating:V1]
 // PLEASE DO NOT MODIFY BECAUSE YOUR CHANGES MAY BE LOST.
