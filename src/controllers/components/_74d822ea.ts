8ff1459cc6a4464648f78a639c436ac8f1088a74
Jatupon Sukkasem <jasukkas.th@gmail.com>
2020-07-08 12:50:49 +0700

    Regenerated project code.

---
+++ src/controllers/components/_74d822ea.ts
@@ -0,0 +1,155 @@
+// Auto[Generating:V1]--->
+// PLEASE DO NOT MODIFY BECAUSE YOUR CHANGES MAY BE LOST.
+
+// Auto[Import]--->
+import {Request, Response} from "express";
+import {SourceType, ActionType, HierarchicalDataTable, HierarchicalDataRow, HierarchicalDataColumn, Input, DatabaseHelper} from '../helpers/DatabaseHelper.js';
+import {ValidationInfo, ValidationHelper} from '../helpers/ValidationHelper.js';
+import {RequestHelper} from '../helpers/RequestHelper.js';
+import {RenderHelper} from '../helpers/RenderHelper.js';
+import {DataTableSchema} from '../helpers/SchemaHelper.js';
+import {Base} from './Base.js';
+
+// <---Auto[Import]
+
+// Import additional modules here:
+// 
+
+// Auto[Declare]--->
+/*enum SourceType {
+  Relational,
+  PrioritizedWorker,
+  Document,
+  VolatileMemory
+}
+enum ActionType {
+  Insert,
+  Update,
+  Delete,
+  Retrieve,
+  Popup,
+  Navigate
+}
+enum ValidationInfo {
+  name: string;
+  required: boolean;
+  customMessage: string;
+}*/
+// <---Auto[Declare]
+
+// Declare private static variables here:
+// 
+
+// Auto[Interface]--->
+/*interface HierarchicalDataTable {
+	source: SourceType;
+	group: string;
+  rows: HierarchicalDataRow[];
+}
+interface HierarchicalDataRow {
+  keys: {[Identifier: string]: HierarchicalDataColumn};
+  columns: {[Identifier: string]: HierarchicalDataColumn};
+  relations: {[Identifier: string]: HierarchicalDataTable};
+}
+interface HierarchicalDataColumn {
+	name: string;
+  value: any;
+}
+interface Input {
+  target: SourceType;
+  group: string;
+  name: string;
+  value: any;
+  guid: string;
+  validation: ValidationInfo;
+}*/
+// <---Auto[Interface]
+
+// Declare or extend interfaces here:
+// 
+
+// Auto[ClassBegin]--->
+class Controller extends Base {
+  constructor(request: Request, response: Response, template: string) {
+  	super(request, response, template);
+  	try {
+	    let [action, schema, data] = this.initialize(request);
+	    this.perform(action, schema, data);
+   	} catch(error) {
+	  	RenderHelper.error(response, error);
+	  }
+  }
+  // <---Auto[ClassBegin]
+  
+  // Declare class variables and functions here:
+  //
+  protected validate(data: Input[]): void {
+  	// The message of thrown error will be the validation message.
+  	//
+ 		ValidationHelper.validate(data);
+  }
+  
+  protected async get(data: Input[]): Promise<{[Identifier: string]: HierarchicalDataTable}> {
+ 		return super.get(data);
+  }
+  
+  protected async post(data: Input[]): Promise<{[Identifier: string]: HierarchicalDataTable}> {
+ 		return super.post(data);
+  }
+  
+  protected async put(data: Input[]): Promise<{[Identifier: string]: HierarchicalDataTable}> {
+ 		return super.put(data);
+  }
+  
+  protected async delete(data: Input[]): Promise<{[Identifier: string]: HierarchicalDataTable}> {
+ 		return super.delete(data);
+  }
+  
+  protected async insert(data: Input[], schema: DataTableSchema): Promise<HierarchicalDataRow[]> {
+ 		return await DatabaseHelper.insert(data, schema);
+  }
+  
+  protected async update(data: Input[], schema: DataTableSchema): Promise<HierarchicalDataRow[]> {
+ 		return await DatabaseHelper.update(data, schema);
+  }
+  
+  protected async remove(data: Input[], schema: DataTableSchema): Promise<HierarchicalDataRow[]> {
+ 		return await DatabaseHelper.delete(data, schema);
+  }
+  
+  protected async retrieve(data: Input[], schema: DataTableSchema): Promise<{[Identifier: string]: HierarchicalDataTable}> {
+ 		return await DatabaseHelper.retrieve(data, schema);
+  }
+  
+  protected async navigate(data: Input[], schema: DataTableSchema): Promise<string> {
+ 		return '/';
+  }
+ 	
+  // Auto[MergingBegin]--->  
+  private initialize(request: Request): [ActionType, DataTableSchema, Input[]] {
+  	let action: ActionType = RequestHelper.getAction(request);
+  	let schema: DataTableSchema = RequestHelper.getSchema(request);
+  	let data: Input[] = [];
+  	let input: Input = null;
+  	
+	  // <---Auto[MergingBegin]
+	  
+	  // Auto[Merging]--->
+	  // <---Auto[Merging]
+	  
+	  // Auto[MergingEnd]--->
+	  
+	  return [action, schema, data];
+	}
+  // <---Auto[MergingEnd]
+  
+  // Auto[ClassEnd]--->
+}
+// <---Auto[ClassEnd]
+
+// Export variables here:
+//
+export default Controller;
+
+// <--- Auto[Generating:V1]
+// PLEASE DO NOT MODIFY BECAUSE YOUR CHANGES MAY BE LOST.
---
+++ src/public/js/components/TextElement_bb6cddae.tsx
@@ -0,0 +1,105 @@
+// Auto[Generating:V1]--->
+// PLEASE DO NOT MODIFY BECAUSE YOUR CHANGES MAY BE LOST.
+
+// Auto[Import]--->
+import {Project, DeclarationHelper} from '../helpers/DeclarationHelper.js';
+import {CodeHelper} from '../helpers/CodeHelper.js';
+import {EventHelper} from '../helpers/EventHelper.js';
+import {DataManipulationHelper} from '../helpers/DataManipulationHelper.js';
+import {IBaseProps, IBaseState, DefaultBaseProps, DefaultBaseState, Base} from './Base.js';
+// <---Auto[Import]
+
+// Import additional modules here:
+//
+
+// Auto[Declare]--->
+
+declare let React: any;
+declare let ReactDOM: any;
+declare let window: any;
+
+// <---Auto[Declare]
+
+// Declare private static variables here:
+//
+
+// Auto[Interface]--->
+interface IAutoBaseProps extends IBaseProps {
+  forward: {classes: String, styles: any};
+}
+interface IAutoBaseState extends IBaseState {
+}
+// <---Auto[Interface]
+
+// Declare or extend interfaces here:
+//
+interface IProps extends IAutoBaseProps {
+  
+}
+interface IState extends IAutoBaseState { 
+}
+
+let DefaultProps = Object.assign({}, DefaultBaseProps, {
+  
+});
+let DefaultState = Object.assign({}, DefaultBaseState, {
+  
+});
+
+// Auto[ClassBegin]--->
+class TextElement_bb6cddae extends Base {
+  state: IState = null;
+  protected static defaultProps: IProps = DefaultProps;
+  
+  constructor(props) {
+    super(props);
+    this.state = CodeHelper.clone(DefaultState);
+    
+    this.initialize();
+  }
+  // <---Auto[ClassBegin]
+  
+  // Declare class variables and functions here:
+  //
+  protected initialize(): void {
+  }
+  
+  // Providing data array base on dot notation:
+  // 
+  protected getDataFromNotation(notation: string): any {
+    return super.getDataFromNotation(notation);
+  }
+  
+  // Auto[Merging]--->
+  // <---Auto[Merging]
+  
+  // Auto[onTextElementClick_bb6cddae:Begin]--->
+  protected onTextElementClick_bb6cddae(event: Event) {
+    // <---Auto[onTextElementClick_bb6cddae:Begin]
+
+    // Handle the event of onTextElementClick (TextElement 1) here:
+    // 
+    window.open('https://github.com/SoftenStorm/boilerplate');
+    
+    // Auto[onTextElementClick_bb6cddae:End]--->
+  }
+  // <---Auto[onTextElementClick_bb6cddae:End]
+
+  // Auto[ClassEnd]--->
+  protected render(): any {
+    return (
+      <div className={"internal-fsb-element col-12 -fsb-self-bb6cddae -fsb-preset-08abd2c9 " + (this.props.forward && this.props.forward.classes || '')} internal-fsb-guid="bb6cddae" style={Object.assign({}, this.props.forward && this.props.forward.styles || {})} onClick={this.onTextElementClick_bb6cddae.bind(this)}>
+        Get Started
+      </div>
+    )
+  }
+}
+DeclarationHelper.declare('Document', 'Controls.TextElement_bb6cddae', TextElement_bb6cddae);
+// <---Auto[ClassEnd]
+
+// Export variables here:
+//
+export {IProps, IState, DefaultProps, DefaultState};
+
+// <--- Auto[Generating:V1]
+// PLEASE DO NOT MODIFY BECAUSE YOUR CHANGES MAY BE LOST.
