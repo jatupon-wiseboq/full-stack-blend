ccf0ab8837d23a3fefe08590c3f2718c9e5e41b6
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-08-14 09:48:43 +0700

    Import Button from Base.js.

--- src/public/js/construction/helpers/FrontEndReactHelper.ts
+++ src/public/js/construction/helpers/FrontEndReactHelper.ts
@@ -65,7 +65,7 @@ import {Project, DeclarationHelper} from '../helpers/DeclarationHelper.js';
 import {CodeHelper} from '../helpers/CodeHelper.js';
 import {EventHelper} from '../helpers/EventHelper.js';
 import {HTMLHelper} from '../helpers/HTMLHelper.js';
-import {IBaseProps, IBaseState, DefaultBaseProps, DefaultBaseState, Base} from './Base.js';
+import {IBaseProps, IBaseState, DefaultBaseProps, DefaultBaseState, Button, Base} from './Base.js';
 // <---Auto[Import]// Auto[Declare]--->
 
 declare let React: any;
