edac50351d1df574978d2a71997ce70c60064de9
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-08-30 12:21:08 +0700

    Fixed: retrieve permissions were combined with modification strictness.

--- src/controllers/helpers/PermissionHelper.ts
+++ src/controllers/helpers/PermissionHelper.ts
@@ -22,106 +22,128 @@ interface Permission {
 
 const PermissionHelper = {
 	allowActionOnTable: async (action: ActionType, schema: DataTableSchema, modifyingColumns: any, session: any=null, data: DataSchema=ProjectConfigurationHelper.getDataSchema()): Promise<boolean> => {
-		if (action == ActionType.Insert || action == ActionType.Upsert) {
-			if (schema.modifyingPermission && schema.modifyingPermission.mode == "relation" && schema.modifyingPermission.relationModeSourceGroup != schema.group) {
-				if (!await PermissionHelper.allowPermission(schema.modifyingPermission, schema, modifyingColumns, session, data)) return false;
-			}
-		} else {
-			if (!await PermissionHelper.allowPermission((action == ActionType.Retrieve) ? schema.retrievingPermission : schema.modifyingPermission, schema, modifyingColumns, session, data)) return false;
-			
-			for (const key in schema.keys) {
-				if (schema.keys.hasOwnProperty(key) && modifyingColumns[key] !== undefined) {
-					if (!await PermissionHelper.allowPermission((action == ActionType.Retrieve) ? schema.keys[key].retrievingPermission : schema.keys[key].modifyingPermission, schema, modifyingColumns, session, data)) return false;
+		return new Promise(async (resolve, reject) => {
+			try {
+				if (action == ActionType.Insert || action == ActionType.Upsert) {
+					if (schema.modifyingPermission && schema.modifyingPermission.mode == "relation" && schema.modifyingPermission.relationModeSourceGroup != schema.group) {
+						if (!await PermissionHelper.allowPermission(schema.modifyingPermission, schema, modifyingColumns, session, data)) {
+							resolve(false);
+							return;
+						}
+					}
+				} else {
+					if (!await PermissionHelper.allowPermission((action == ActionType.Retrieve) ? schema.retrievingPermission : schema.modifyingPermission, schema, modifyingColumns, session, data)) {
+						resolve(false);
+						return;
+					} else if (action != ActionType.Retrieve) {
+						for (const key in schema.keys) {
+							if (schema.keys.hasOwnProperty(key) && modifyingColumns[key] !== undefined) {
+								if (!await PermissionHelper.allowPermission((action == ActionType.Retrieve) ? schema.keys[key].retrievingPermission : schema.keys[key].modifyingPermission, schema, modifyingColumns, session, data)) {
+									resolve(false);
+									return;
+								}
+							}
+						}
+						for (const key in schema.columns) {
+							if (schema.columns.hasOwnProperty(key) && modifyingColumns[key] !== undefined) {
+								if (!await PermissionHelper.allowPermission((action == ActionType.Retrieve) ? schema.columns[key].retrievingPermission : schema.columns[key].modifyingPermission, schema, modifyingColumns, session, data)) {
+									resolve(false);
+									return;
+								}
+							}
+						}
+					}
 				}
+				
+				resolve(true);
+			} catch(error) {
+				reject(error);
 			}
-			for (const key in schema.columns) {
-				if (schema.columns.hasOwnProperty(key) && modifyingColumns[key] !== undefined) {
-					if (!await PermissionHelper.allowPermission((action == ActionType.Retrieve) ? schema.columns[key].retrievingPermission : schema.columns[key].modifyingPermission, schema, modifyingColumns, session, data)) return false;
-				}
-			}
-		}
-		
-		return true;
+		});
 	},
 	allowOutputOfColumn: async (column: DataColumnSchema, schema: DataTableSchema, modifyingColumns: any, session: any=null, data: DataSchema=ProjectConfigurationHelper.getDataSchema()): Promise<boolean> => {
 		return PermissionHelper.allowPermission(column.retrievingPermission, schema, modifyingColumns, session, data);
 	},
 	allowPermission: async (permission: Permission, target: DataTableSchema, modifyingColumns, session: any=null, data: DataSchema=ProjectConfigurationHelper.getDataSchema()): Promise<boolean> => {
 		return new Promise((resolve, reject) => {
-			if (permission == null) {
-				resolve(true);
-				return;
-			}
-			
-			switch (permission.mode) {
-				case "block":
-				case "always":
-					resolve(false);
-					break;
-				case "relation":
-					if (session == null) throw new Error("There was an error authorizing a permission (the request session variable was null).");
-					
-					const shortestPath = SchemaHelper.findShortestPathOfRelations(target, data.tables[permission.relationModeSourceGroup], data);
-					let value;
-					
-					switch (permission.relationMatchingMode) {
-						case "session":
-							value = session[permission.relationMatchingSessionName];
-							break;
-						default:
-							value = permission.relationMatchingConstantValue;
-							break;
-					}
-					
-					const INNER_JOIN = [];
-					const WHERE_CLAUSE = [];
-					const VALUES = [];
-					
-					let current = shortestPath[0];
-					for (let i=1; i<shortestPath.length; i++) {
-						const next = shortestPath[i];
-						
-						INNER_JOIN.push(`INNER JOIN ${next.group} ON ${current.group}.${current.relations[next.group].sourceEntity} = ${next.group}.${current.relations[next.group].targetEntity}`);
-						
-						current = next;
-					}
-					
-					WHERE_CLAUSE.push(`CONVERT(${permission.relationModeSourceGroup}.${permission.relationModeSourceEntity}, char) = CONVERT(?, char)`);
-					VALUES.push(value);
-					
-					const from = shortestPath[shortestPath.length - 1];
-					for (const key in from.keys) {
-						if (from.keys.hasOwnProperty(key) && modifyingColumns[key] !== undefined) {
-							WHERE_CLAUSE.push(`${from.group}.${key} = ?`);
-							VALUES.push(modifyingColumns[key]);
-						}
-					}
-					
-					const COMMAND = `SELECT * FROM ${permission.relationModeSourceGroup} ${INNER_JOIN.join(" ")} WHERE ${WHERE_CLAUSE.join(" AND ")} LIMIT 1`;
-      		console.log(COMMAND);
-      		
-      		const cachedPermissionMD5Key = Md5.init(session.id + COMMAND);
-      		if (cachedPermissions[cachedPermissionMD5Key]) return cachedPermissions[cachedPermissionMD5Key];
-					
-					RelationalDatabaseClient.query(COMMAND, VALUES, (function(error, results, fields) {
-            if (error) {
-              reject(error);
-      			} else if (results.length > 0) {
-      				cachedPermissions[cachedPermissionMD5Key] = true;
-      			  resolve(true);
-      			} else {
-      				cachedPermissions[cachedPermissionMD5Key] = false;
-      			  resolve(false);
-      			}
-      		}).bind(this));
-					break;
-				case "session":
-					if (session == null) throw new Error("There was an error authorizing a permission (the request session variable was null).");
-					resolve(session[permission.sessionMatchingSessionName] == permission.sessionMatchingConstantValue);
-					break;
-				default:
+			try {
+				if (permission == null) {
 					resolve(true);
-					break;
+					return;
+				}
+				
+				switch (permission.mode) {
+					case "block":
+					case "always":
+						resolve(false);
+						break;
+					case "relation":
+						if (session == null) throw new Error("There was an error authorizing a permission (the request session variable was null).");
+						
+						const shortestPath = SchemaHelper.findShortestPathOfRelations(target, data.tables[permission.relationModeSourceGroup], data);
+						let value;
+						
+						switch (permission.relationMatchingMode) {
+							case "session":
+								value = session[permission.relationMatchingSessionName];
+								break;
+							default:
+								value = permission.relationMatchingConstantValue;
+								break;
+						}
+						
+						const INNER_JOIN = [];
+						const WHERE_CLAUSE = [];
+						const VALUES = [];
+						
+						let current = shortestPath[0];
+						for (let i=1; i<shortestPath.length; i++) {
+							const next = shortestPath[i];
+							
+							INNER_JOIN.push(`INNER JOIN ${next.group} ON ${current.group}.${current.relations[next.group].sourceEntity} = ${next.group}.${current.relations[next.group].targetEntity}`);
+							
+							current = next;
+						}
+						
+						WHERE_CLAUSE.push(`CONVERT(${permission.relationModeSourceGroup}.${permission.relationModeSourceEntity}, char) = CONVERT(?, char)`);
+						VALUES.push(value);
+						
+						const from = shortestPath[shortestPath.length - 1];
+						for (const key in from.keys) {
+							if (from.keys.hasOwnProperty(key) && modifyingColumns[key] !== undefined) {
+								WHERE_CLAUSE.push(`${from.group}.${key} = ?`);
+								VALUES.push(modifyingColumns[key]);
+							}
+						}
+						
+						const COMMAND = `SELECT * FROM ${permission.relationModeSourceGroup} ${INNER_JOIN.join(" ")} WHERE ${WHERE_CLAUSE.join(" AND ")} LIMIT 1`;
+	      		console.log(COMMAND);
+	      		
+	      		const cachedPermissionMD5Key = Md5.init(session.id + COMMAND);
+	      		if (cachedPermissions[cachedPermissionMD5Key]) return cachedPermissions[cachedPermissionMD5Key];
+						
+						RelationalDatabaseClient.query(COMMAND, VALUES, (function(error, results, fields) {
+	            if (error) {
+	              reject(error);
+	      			} else if (results.length > 0) {
+	      				cachedPermissions[cachedPermissionMD5Key] = true;
+	      			  resolve(true);
+	      			} else {
+	      				cachedPermissions[cachedPermissionMD5Key] = false;
+	      			  resolve(false);
+	      			}
+	      		}).bind(this));
+						break;
+					case "session":
+						if (session == null) throw new Error("There was an error authorizing a permission (the request session variable was null).");
+						resolve(session[permission.sessionMatchingSessionName] == permission.sessionMatchingConstantValue);
+						break;
+					default:
+						resolve(true);
+						break;
+				}
+			} catch(error) {
+				reject(error);
 			}
 		});
 	},
