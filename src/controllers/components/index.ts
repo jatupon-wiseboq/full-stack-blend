e49819481b7c4018a01cc222994eff726ddd77b7
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-09-24 13:54:12 +0700

    [test saving]

--- src/controllers/components/index.ts
+++ src/controllers/components/index.ts
@@ -1,21 +1,19 @@
 // Auto[Generating:V1]--->
 // PLEASE DO NOT MODIFY BECAUSE YOUR CHANGES MAY BE LOST.
 
-
 // Auto[Import]--->
 import {Request, Response} from "express";
-import {SourceType, ActionType, HierarchicalDataTable, HierarchicalDataRow, Input, DatabaseHelper} from "./../helpers/DatabaseHelper.js";
-import {ValidationInfo, ValidationHelper} from "./../helpers/ValidationHelper.js";
-import {RequestHelper} from "./../helpers/RequestHelper.js";
-import {RenderHelper} from "./../helpers/RenderHelper.js";
-import {DataTableSchema} from "./../helpers/SchemaHelper.js";
-import {Base} from "./Base.js";
+import {SourceType, ActionType, HierarchicalDataTable, HierarchicalDataRow, Input, DatabaseHelper} from './../helpers/DatabaseHelper.js';
+import {ValidationInfo, ValidationHelper} from './../helpers/ValidationHelper.js';
+import {RequestHelper} from './../helpers/RequestHelper.js';
+import {RenderHelper} from './../helpers/RenderHelper.js';
+import {DataTableSchema} from './../helpers/SchemaHelper.js';
+import {Base} from './Base.js';
 
 // <---Auto[Import]
 
 // Import additional modules here:
 // 
-
 
 // Auto[Declare]--->
 /*enum SourceType {
@@ -43,7 +41,6 @@ enum ValidationInfo {
 // Declare private static variables here:
 // 
 
-
 // Auto[Interface]--->
 /*interface HierarchicalDataTable {
 	source: SourceType;
@@ -69,13 +66,12 @@ interface Input {
 // Declare or extend interfaces here:
 // 
 
-
 // Auto[ClassBegin]--->
 class Controller extends Base {
   constructor(request: Request, response: Response, template: string) {
   	super(request, response, template);
   	try {
-	    const [action, schema, data] = this.initialize(request);
+	    let [action, schema, data] = this.initialize(request);
 	    this.perform(action, schema, data);
    	} catch(error) {
 	  	RenderHelper.error(response, error);
@@ -158,7 +154,7 @@ class Controller extends Base {
   protected async insert(data: Input[], schema: DataTableSchema): Promise<HierarchicalDataRow[]> {
     return new Promise(async (resolve, reject) => {
       try {
-      	const options = RequestHelper.getOptions(this.pageId, this.request);
+      	let options = RequestHelper.getOptions(this.pageId, this.request);
         resolve(await DatabaseHelper.insert(data, schema, options.crossRelationUpsert, this.request.session));
       } catch(error) {
         reject(error);
@@ -169,7 +165,7 @@ class Controller extends Base {
   protected async update(data: Input[], schema: DataTableSchema): Promise<HierarchicalDataRow[]> {
     return new Promise(async (resolve, reject) => {
     	try {
-      	const options = RequestHelper.getOptions(this.pageId, this.request);
+      	let options = RequestHelper.getOptions(this.pageId, this.request);
         resolve(await DatabaseHelper.update(data, schema, options.crossRelationUpsert, this.request.session));
       } catch(error) {
         reject(error);
@@ -200,7 +196,7 @@ class Controller extends Base {
   protected async retrieve(data: Input[], schema: DataTableSchema): Promise<{[Identifier: string]: HierarchicalDataTable}> {
     return new Promise(async (resolve, reject) => {
     	try {
-      	const options = RequestHelper.getOptions(this.pageId, this.request);
+      	let options = RequestHelper.getOptions(this.pageId, this.request);
         resolve(await DatabaseHelper.retrieve(data, schema, this.request.session, options.enabledRealTimeUpdate));
       } catch(error) {
         reject(error);
@@ -219,13 +215,11 @@ class Controller extends Base {
     });
   }
  	
-  
- 	
   // Auto[MergingBegin]--->  
   private initialize(request: Request): [ActionType, DataTableSchema, Input[]] {
-  	const schema: DataTableSchema = RequestHelper.getSchema(this.pageId, request);
-  	const data: Input[] = [];
-  	const input: Input = null;
+  	let schema: DataTableSchema = RequestHelper.getSchema(this.pageId, request);
+  	let data: Input[] = [];
+  	let input: Input = null;
   	
 	  // <---Auto[MergingBegin]
 	  
@@ -238,7 +232,7 @@ class Controller extends Base {
 	  
 	  // Auto[MergingEnd]--->
 	  
-  	const action: ActionType = RequestHelper.getAction(this.pageId, request);
+  	let action: ActionType = RequestHelper.getAction(this.pageId, request);
 	  return [action, schema, data];
 	}
   // <---Auto[MergingEnd]
@@ -250,7 +244,6 @@ class Controller extends Base {
 // Export variables here:
 //
 export default Controller;
-
 
 // <--- Auto[Generating:V1]
 // PLEASE DO NOT MODIFY BECAUSE YOUR CHANGES MAY BE LOST.
