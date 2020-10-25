440bf6bbce41ad94e3e5503fcf5cfb66d49c466c
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-10-25 15:17:56 +0700

    Implement animation helper.

---
+++ src/public/js/helpers/AnimationHelper.ts
@@ -0,0 +1,45 @@
+// Auto[Generating:V1]--->
+// PLEASE DO NOT MODIFY BECUASE YOUR CHANGES MAY BE LOST.
+
+import {HTMLHelper} from './HTMLHelper.js';
+
+const AnimationHelper = {
+	// Document Object Model (DOM) Queries
+	// 
+  add: (animations: any) => {
+    let container = HTMLHelper.getElementByAttributeNameAndValue('internal-fsb-guid', '0');
+    if (container) {
+    	let currentAnimations = (HTMLHelper.getAttribute(container, 'internal-fsb-animation') || '').split(' ');
+    	
+    	for (let animation of animations) {
+    		if (currentAnimations.indexOf(animation) == -1) {
+    			currentAnimations.append(animation);
+    		}
+    	}
+    	
+    	currentAnimations = currentAnimations.filter(currentAnimation => currentAnimation != '');
+    	HTMLHelper.setAttribute(container, 'internal-fsb-animation', currentAnimations.join(' '));
+    }
+  },
+  remove: (animations: any) => {
+    let container = HTMLHelper.getElementByAttributeNameAndValue('internal-fsb-guid', '0');
+    if (container) {
+    	let currentAnimations = (HTMLHelper.getAttribute(container, 'internal-fsb-animation') || '').split(' ');
+    	
+    	for (let animation of animations) {
+    		let index = currentAnimations.indexOf(animation);
+    		if (index != -1) {
+    			currentAnimations.splice(index, 1);
+    		}
+    	}
+    	
+    	currentAnimations = currentAnimations.filter(currentAnimation => currentAnimation != '');
+    	HTMLHelper.setAttribute(container, 'internal-fsb-animation', currentAnimations.join(' '));
+    }
+  }
+}
+
+export {AnimationHelper};
+
+// <--- Auto[Generating:V1]
+// PLEASE DO NOT MODIFY BECUASE YOUR CHANGES MAY BE LOST.
