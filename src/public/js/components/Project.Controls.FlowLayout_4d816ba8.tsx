b7718066968c335d481b368c5512e44a616164a7
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-11-09 19:48:52 +0700

    Apply css overflow: hidden for authentication dialog.

--- src/public/js/components/Project.Controls.FlowLayout_4d816ba8.tsx
+++ src/public/js/components/Project.Controls.FlowLayout_4d816ba8.tsx
@@ -116,9 +116,9 @@ class FlowLayout_4d816ba8 extends Base {
     
   }
 
-  protected onButtonSubmitted_954a291a(event: Event) {
+  protected onButtonFailed_954a291a(event: Event) {
 
-    // Handle the event of onButtonSubmitted (Button 3) here:
+    // Handle the event of onButtonFailed (Button 3) here:
     // 
     this.setState({submitting: false});
     
@@ -132,9 +132,9 @@ class FlowLayout_4d816ba8 extends Base {
     
   }
 
-  protected onButtonSubmitted_b2b66792(event: Event) {
+  protected onButtonFailed_b2b66792(event: Event) {
 
-    // Handle the event of onButtonSubmitted (Button 1) here:
+    // Handle the event of onButtonFailed (Button 1) here:
     // 
     this.setState({submitting: false});
     
@@ -147,11 +147,11 @@ class FlowLayout_4d816ba8 extends Base {
       div(style=Object.assign({'FsbInheritedPresets': '245bc127'}, this.props.forward && this.props.forward.styles || {}), className="-fsb-preset-245bc127 internal-fsb-element " + (this.props.forward && this.props.forward.classes || ''), internal-fsb-guid="4d816ba8")
         .container-fluid
           .internal-fsb-allow-cursor.internal-fsb-strict-layout.row
-            .col-6.internal-fsb-allow-cursor.internal-fsb-element.offset-0(style={'MsFlexDirection': 'column', 'WebkitFlexDirection': 'column', 'background': 'rgba(3, 115, 252, 1)', 'bottom': '0px', 'display': 'flex', 'flexDirection': 'column', 'left': '0px', 'paddingBottom': '15px', 'paddingTop': '15px', 'position': 'absolute', 'top': '0px'}, internal-fsb-guid="257894ed")
-              .internal-fsb-element(style={'color': 'rgba(255, 255, 255, 1)', 'fontSize': '24px'}, internal-fsb-guid="08a05b72")
+            .col-6.internal-fsb-allow-cursor.internal-fsb-element.offset-0(style={'MsFlexDirection': 'column', 'MsOverflowX': 'hidden', 'MsOverflowY': 'auto', 'WebkitFlexDirection': 'column', 'background': 'rgba(3, 115, 252, 1)', 'bottom': '0px', 'display': 'flex', 'flexDirection': 'column', 'left': '0px', 'overflowX': 'hidden', 'overflowY': 'auto', 'paddingBottom': '15px', 'paddingTop': '15px', 'position': 'absolute', 'top': '0px'}, internal-fsb-guid="257894ed")
+              .internal-fsb-element(style={'color': 'rgba(255, 255, 255, 1)', 'display': 'block', 'fontSize': '24px', 'width': '100%'}, internal-fsb-guid="08a05b72")
                 | StackBlend Studio 2020
-              .internal-fsb-allow-cursor.internal-fsb-element(style={'WebkitFlexGrow': '1', 'flexGrow': '1'}, internal-fsb-guid="e80dd7c1")
-              .internal-fsb-element(style={'color': 'rgba(255, 255, 255, 1)', 'fontSize': '13px'}, internal-fsb-guid="e55072d1")
+              .internal-fsb-allow-cursor.internal-fsb-element(style={'WebkitFlexGrow': '1', 'flexGrow': '1', 'width': '100%'}, internal-fsb-guid="e80dd7c1")
+              .internal-fsb-element(style={'color': 'rgba(255, 255, 255, 1)', 'display': 'block', 'fontSize': '12px', 'width': '100%'}, internal-fsb-guid="e55072d1")
                 | By accessing the website, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.
             .col-6.internal-fsb-allow-cursor.internal-fsb-element.offset-6(style={'MsFlexDirection': 'column', 'MsOverflowX': 'hidden', 'MsOverflowY': 'auto', 'WebkitFlexDirection': 'column', 'bottom': '0px', 'display': 'flex', 'flexDirection': 'column', 'overflowX': 'hidden', 'overflowY': 'auto', 'position': 'absolute', 'right': '0px', 'top': '0px'}, internal-fsb-guid="b20bb476")
               .internal-fsb-allow-cursor.internal-fsb-element(style={'WebkitFlexGrow': '1', 'flexGrow': '1'}, internal-fsb-guid="73b5e922")
@@ -159,7 +159,7 @@ class FlowLayout_4d816ba8 extends Base {
                 .container-fluid
                   .internal-fsb-allow-cursor.internal-fsb-strict-layout.row
                     .col-12.internal-fsb-element(style={'color': 'rgba(166, 166, 166, 1)', 'fontSize': '13px', 'marginBottom': '15px', 'textAlign': 'center'}, internal-fsb-guid="e439a68e")
-                      | Please login or signup to start using StackBlend studio
+                      | Please login or signup to start using StackBlend Studio
                     .col-12.internal-fsb-allow-cursor.internal-fsb-element(style={'marginBottom': '15px', 'paddingLeft': '0px', 'paddingRight': '15px'}, internal-fsb-guid="89972713")
                       .container-fluid
                         .internal-fsb-allow-cursor.internal-fsb-strict-layout.row
@@ -178,10 +178,10 @@ class FlowLayout_4d816ba8 extends Base {
                         .internal-fsb-allow-cursor.internal-fsb-strict-layout.row
                           .col-10.internal-fsb-element.offset-1(style={padding: '0px'}, internal-fsb-guid="d3de6c93")
                             input.form-control.form-control-sm(style={'display': 'block', 'height': '34px', 'marginTop': '10px', 'width': '100%'}, placeholder="Confirm password", type="password", disabled=this.state.submitting, required=true)
-                    Button.btn.btn-md.btn-primary.col-6.internal-fsb-allow-cursor.internal-fsb-element.offset-3(style={'marginTop': '10px', display: (()=>{return (this.state.currentTab == 0) ? 'block' : 'none';})()}, onClick=((event) => { window.internalFsbSubmit('954a291a', 'User', event, ((results) => { this.manipulate('954a291a', 'User', results); }).bind(this)); }).bind(this), disabled=this.state.submitting, type="button", onSubmitted=this.onButtonSubmitted_954a291a.bind(this), onSubmitting=this.onButtonSubmitting_954a291a.bind(this), internal-fsb-guid="954a291a")
+                    Button.btn.btn-md.btn-primary.col-6.internal-fsb-allow-cursor.internal-fsb-element.offset-3(style={'marginTop': '10px', display: (()=>{return (this.state.currentTab == 0) ? 'block' : 'none';})()}, onClick=((event) => { window.internalFsbSubmit('954a291a', 'User', event, ((results) => { this.manipulate('954a291a', 'User', results); }).bind(this)); }).bind(this), disabled=this.state.submitting, type="button", onFailed=this.onButtonFailed_954a291a.bind(this), onSubmitting=this.onButtonSubmitting_954a291a.bind(this), internal-fsb-guid="954a291a")
                       .internal-fsb-element(internal-fsb-guid="954a291a-text")
                         | Continue
-                    Button.btn.btn-md.btn-primary.col-6.internal-fsb-allow-cursor.internal-fsb-element.offset-3(style={'marginTop': '10px', display: (()=>{return (this.state.currentTab == 1) ? 'block' : 'none';})()}, onClick=((event) => { window.internalFsbSubmit('b2b66792', 'User', event, ((results) => { this.manipulate('b2b66792', 'User', results); }).bind(this)); }).bind(this), disabled=this.state.submitting, type="button", onSubmitted=this.onButtonSubmitted_b2b66792.bind(this), onSubmitting=this.onButtonSubmitting_b2b66792.bind(this), internal-fsb-guid="b2b66792")
+                    Button.btn.btn-md.btn-primary.col-6.internal-fsb-allow-cursor.internal-fsb-element.offset-3(style={'marginTop': '10px', display: (()=>{return (this.state.currentTab == 1) ? 'block' : 'none';})()}, onClick=((event) => { window.internalFsbSubmit('b2b66792', 'User', event, ((results) => { this.manipulate('b2b66792', 'User', results); }).bind(this)); }).bind(this), disabled=this.state.submitting, type="button", onFailed=this.onButtonFailed_b2b66792.bind(this), onSubmitting=this.onButtonSubmitting_b2b66792.bind(this), internal-fsb-guid="b2b66792")
                       .internal-fsb-element(internal-fsb-guid="b2b66792-text")
                         | Continue
               .internal-fsb-allow-cursor.internal-fsb-element(style={'WebkitFlexGrow': '1', 'flexGrow': '1'}, internal-fsb-guid="d5903637")
