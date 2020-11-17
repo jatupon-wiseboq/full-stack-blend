b09b1fcaaed0c169f4c726a104c4ef009cb79660
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-11-17 10:22:49 +0700

    Clear unused code and create the example of an env file.

---
+++ src/controllers/user.ts
@@ -0,0 +1,80 @@
+import async from "async";
+import crypto from "crypto";
+import nodemailer from "nodemailer";
+import passport from "passport";
+import {User, UserDocument, AuthToken} from "../models/User";
+import {Request, Response, NextFunction} from "express";
+import {IVerifyOptions} from "passport-local";
+import {WriteError} from "mongodb";
+import {check, sanitize, validationResult} from "express-validator";
+import "../config/passport";
+import "babel-polyfill";
+
+/**
+ * GET /logout
+ * Log out.
+ */
+export const logout = (req: Request, res: Response) => {
+
+    req.logout();
+    res.redirect("/");
+
+};
+
+/**
+ * GET /account/delete
+ * Delete user account.
+ */
+
+export const getDeleteAccount = (req: Request, res: Response, next: NextFunction) => {
+
+    const user = req.user as UserDocument;
+
+    User.remove({_id: user.id}, (err) => {
+
+        if (err) {
+
+            return next(err);
+
+        }
+        req.logout();
+        req.flash("info", {msg: "Your account has been deleted."});
+        res.redirect("/");
+
+    });
+
+};
+
+/**
+ * GET /account/unlink/:provider
+ * Unlink OAuth provider.
+ */
+export const getOauthUnlink = (req: Request, res: Response, next: NextFunction) => {
+
+    const {provider} = req.params;
+    const user = req.user as UserDocument;
+
+    User.findById(user.id, (err, user: any) => {
+
+        if (err) {
+
+            return next(err);
+
+        }
+        user[provider] = undefined;
+        user.tokens = user.tokens.filter((token: AuthToken) => token.kind !== provider);
+        user.save((err: WriteError) => {
+
+            if (err) {
+
+                return next(err);
+
+            }
+            req.flash("info", {msg: `${provider} account has been unlinked.`});
+            res.redirect("/account/settings");
+
+        });
+
+    });
+
+};
