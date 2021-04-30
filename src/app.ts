bd62866b37f5d437d50994bcf3954ab395ea3f53
Jatupon Sukkasem <jasukkas@Akes-Macintosh.local>
2021-04-30 20:53:08 +0700

    Serve polyfill.io under the same domain to ease non-SNI configuration.

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ import bluebird from "bluebird";
 import cors from "cors";
 import errorHandler from "errorhandler";
 import dotenv from "dotenv";
+import polyfill from "polyfill-library";
 
 const MongoStore = mongo(session);
 
@@ -99,6 +100,23 @@ app.use(
 if (["production"].indexOf(process.env.NODE_ENV) == -1) {
   app.use(errorHandler());
 }
+
+// Serve polyfill.io under the same domain to ease non-SNI configuration.
+// 
+app.get('/js/libraries/polyfills/polyfill.io.js', (req, res) => {
+  polyfill.getPolyfillString({
+    uaString: req.get('User-Agent'),
+    minify: true,
+    features: {
+      'es5': { flags: ['gated'] },
+      'es6': { flags: ['gated'] },
+      'es7': { flags: ['gated'] }
+    }
+  }).then(function(bundle) {
+    res.set('Content-Type', 'text/javascript');
+  	res.send(Buffer.from(bundle));
+  });
+});
 
 // StackBlend code editor's endpoint
 // 
