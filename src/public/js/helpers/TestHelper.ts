ffad05602caa75fa792dbfd436fd970aae7dbe32
Jatupon Sukkasem <jatupon@softenstorm.com>
2021-05-12 09:13:42 +0700

    Add test helper.

---
+++ src/public/js/helpers/TestHelper.ts
@@ -0,0 +1,50 @@
+// Auto[Generating:V1]--->
+// PLEASE DO NOT MODIFY BECUASE YOUR CHANGES MAY BE LOST.
+
+import {HTMLHelper} from './HTMLHelper';
+
+const TestHelper = {
+  identify: (delay: number=1000) => {
+  	window.setTimeout(() => {
+  		if (!HTMLHelper.getElementById('selenium-ide-indicator')) return;
+  		
+  		const elements = HTMLHelper.getElementsByAttribute('internal-fsb-guid');
+  		
+  		for (const element of elements) {
+  			TestHelper.recursiveAssignId(element);
+  		}
+  	}, delay);
+  },
+  recursiveAssignId: (element: any, guid: string='_', suffix: string='') => {
+  	if (!element.tagName) return;
+  	
+  	let currentId = HTMLHelper.getAttribute(element, 'id') || '';
+  	
+  	if (currentId.indexOf('internal-fsb-') == 0) currentId = '';
+  	
+  	const _guid = HTMLHelper.getAttribute(element, 'internal-fsb-guid');
+  	guid = _guid || guid;
+  	
+  	if (_guid) {
+	  	const elements = HTMLHelper.getElementsByAttributeNameAndValue('internal-fsb-guid', _guid, element.parentNode);
+	  	const index = elements.indexOf(element);
+	  	
+	  	if (index == 0) suffix = '-first';
+	  	else if (index == elements.length-1) suffix = '-last';
+	  	else if (index != -1) suffix = `-${index}`;
+	  }
+  	
+  	if (!currentId) HTMLHelper.setAttribute(element, 'id', `internal-fsb-${guid}${suffix}`);
+  	
+  	const children = element.children;
+  	
+  	for (let i=0; i<children.length; i++) {
+  		TestHelper.recursiveAssignId(children[i], `${guid}-${i}`);
+  	}
+  }
+}
+
+export {TestHelper};
+
+// <--- Auto[Generating:V1]
+// PLEASE DO NOT MODIFY BECUASE YOUR CHANGES MAY BE LOST.
