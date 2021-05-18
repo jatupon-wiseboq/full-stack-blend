fc5ec329e1278f782b4e62ab024f681e47d61b01
Jatupon Sukkasem <jasukkas@Akes-Macintosh.local>
2021-05-18 14:01:19 +0700

    Bring redlines back for selector mode.

--- src/public/js/construction/editor/Editor.tsx
+++ src/public/js/construction/editor/Editor.tsx
@@ -225,6 +225,7 @@ let cachedUpdateEditorProperties = {};
 	      	}
 	      }
 	      if (content && content['extensions']) {
+	      	document.body.setAttribute('selector', (content['extensions']['editingAnimationID'] == 'selector') ? 'true' : 'false');
 	      	if (content['extensions']['editorCurrentMode']) document.body.setAttribute('mode', content['extensions']['editorCurrentMode']);
 	      	
 	      	for (let key of ['editorCurrentMode', 'hasParentReactComponent', 'editing', 'editingAnimationID', 'editingKeyframeID', 'areFormatAndStyleOptionsAvailable', 'animationGroupMode', 'animationRepeatMode', 'isTableLayoutRow', 'isFirstElementOfComponent']) {
@@ -296,10 +297,14 @@ let cachedUpdateEditorProperties = {};
   });
   
   window.addEventListener("message", (event) => {
-    let data = JSON.parse(event.data);
-  	if (data.target == 'editor') {
-    	synchronize(data.name, data.content);
-    }
+  	try {
+	    let data = JSON.parse(event.data);
+	  	if (data.target == 'editor') {
+	    	synchronize(data.name, data.content);
+	    }
+	  } catch (error) {
+	  	/* void */
+	  }
   });
   
   window.addEventListener("beforeunload", (event: any) => {
