d0e02d4d9d465bf30a9a917a2f78bc58880f89f5
Jatupon Sukkasem <jasukkas@Jatupons-iMac.local>
2021-08-22 19:00:53 +0700

    [continue] Fixed: cannot create directory 'dist/public/': No such file or directory.

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,12 +1,12 @@
 import * as shell from "shelljs";
 import fs from "fs";
 
-shell.cp("-R", "src/public/fonts", "dist/public/");
-shell.cp("-R", "src/public/images", "dist/public/");
 shell.mkdir("dist");
 shell.mkdir("dist/public");
 shell.mkdir("dist/public/js");
 shell.mkdir("dist/public/css");
+shell.cp("-R", "src/public/fonts", "dist/public/");
+shell.cp("-R", "src/public/images", "dist/public/");
 shell.cp("-R", "src/public/js/*", "dist/public/js/");
 shell.cp("-R", "node_modules/ruffle-mirror/*", "dist/public/js/");
 shell.cp("-R", "src/public/css/*", "dist/public/css/");
