06a831111dfe9e75762d7a0c19dc6e02953823c3
Jatupon Sukkasem <jasukkas@Jatupons-iMac.local>
2021-08-22 18:52:50 +0700

    Remove unused archiving code.

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,6 +1,5 @@
 import * as shell from "shelljs";
 import fs from "fs";
-import archiver from "archiver";
 
 shell.cp("-R", "src/public/fonts", "dist/public/");
 shell.cp("-R", "src/public/images", "dist/public/");
@@ -9,18 +8,6 @@ shell.mkdir("dist/public/css/");
 shell.cp("-R", "src/public/js/*", "dist/public/js/");
 shell.cp("-R", "node_modules/ruffle-mirror/*", "dist/public/js/");
 shell.cp("-R", "src/public/css/*", "dist/public/css/");
-
-if (shell.ls('boilerplate').length == 0) {
-  shell.exec("git clone https://github.com/SoftenStorm/boilerplate.git");
-}
-
-const archive = archiver("zip", { zlib: { level: 9 }});
-const stream = fs.createWriteStream("dist/public/boilerplate.v1.zip");
-
-archive
-	.directory("boilerplate/src", false)
-  .pipe(stream);
-archive.finalize();
 
 if (shell.ls("localhost.crt").length == 0) {
   shell.exec("openssl req \
