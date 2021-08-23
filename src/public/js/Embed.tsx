9fd1d0dcb8907834acda95ce4d56f9f41cf48496
Jatupon Sukkasem <jasukkas@Jatupons-iMac.local>
2021-08-23 18:05:20 +0700

    Fixed: couldn't import Ruffle.

--- src/public/js/Embed.tsx
+++ src/public/js/Embed.tsx
@@ -1,7 +1,7 @@
 import {DataManipulationHelper} from './helpers/DataManipulationHelper';
 import {TestHelper} from './helpers/TestHelper';
 import {AnimationHelper} from './helpers/AnimationHelper';
-import * as Ruffle from './ruffle.js';
+import * as Ruffle from '../../../dist/public/js/ruffle.js';
 
 declare let window: any;
 
