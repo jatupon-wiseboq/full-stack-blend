545516562838e470208dc186402f95d9332ecd1a
Jatupon Sukkasem <jasukkas@Jatupons-iMac.local>
2021-09-06 13:23:10 +0700

    [continue] Recursive update front-end and back-end code for all referencing pages, sites, and components while saving.

--- src/public/js/construction/area/helpers/WorkspaceHelper.ts
+++ src/public/js/construction/area/helpers/WorkspaceHelper.ts
@@ -923,7 +923,7 @@ var WorkspaceHelper = {
     }
   },
   generateBackEndCodeForID: (id: string=InternalProjectSettings.editingPageID) => {
-  	if ('site' == InternalProjectSettings.currentMode && WorkspaceHelper.getCurrentGenerateFrontEndKey() == InternalProjectSettings.editingPageID) {
+  	if ('site' == InternalProjectSettings.currentMode && id == InternalProjectSettings.editingPageID) {
     	return WorkspaceHelper.generateBackEndCodeForPage('site', HTMLHelper.getElementByAttributeNameAndValue("internal-fsb-guid", "0", window.document.body));
     } else {
 	  	document.body.appendChild(temp);
