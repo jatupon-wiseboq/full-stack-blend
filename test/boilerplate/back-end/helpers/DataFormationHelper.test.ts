198178bf0ba845a4b97ac7e74dbe4d7ffb06e73c
Jatupon Sukkasem <jasukkas@outlook.com>
2021-11-06 18:01:18 +0700

    There is a case of column type (keys, columns.)

--- test/boilerplate/back-end/helpers/DataFormationHelper.test.ts
+++ test/boilerplate/back-end/helpers/DataFormationHelper.test.ts
@@ -22,22 +22,22 @@ describe('DataFormationHelper', () => {
 	test('Complex Structure', () => {
 		let data = null;
 		data = {
-			a: 1,
+			$a: 1,
 			b: [0, 1],
-			c: {a: 1},
+			c: {$a: 1},
 			d: {b: [0, 1]},
-			e: {c: {a: 1}, d: [0, 1]},
-			f: {a: [[0, 1, 2]], b: [{a: 1, b: [0, 1]}, {a: 2, b: [0, 2]}, {a: 3, b: [0, 3]}]}
+			e: {c: {$a: 1}, d: [0, 1]},
+			f: {a: [[0, 1, 2]], b: [{$a: 1, b: [0, 1]}, {$a: 2, b: [0, 2]}, {$a: 3, b: [0, 3]}]}
 		};
 		expect(DataFormationHelper.convertFromHierarchicalDataTableToJSON(DataFormationHelper.convertFromJSONToHierarchicalDataTable(data))).toEqual(data);
 		
 		data = {
-			a: null,
+			$a: null,
 			b: [0.0, undefined],
-			c: {a: 1.125},
+			c: {$a: 1.125},
 			d: {b: [NaN, Number.MAX_SAFE_INTEGER]},
-			e: {c: {a: ''}, d: [0, 1]},
-			f: {a: [[0, 'undefined', 'null']], b: [{a: Infinity, b: [0, -Infinity]}, {a: '', b: [null, '']}, {a: '3.00541', b: ['0', 3]}]}
+			e: {c: {$a: ''}, d: [0, 1]},
+			f: {a: [[0, 'undefined', 'null']], b: [{$a: Infinity, b: [0, -Infinity]}, {$a: '', b: [null, '']}, {$a: '3.00541', b: ['0', 3]}]}
 		};
 		expect(DataFormationHelper.convertFromHierarchicalDataTableToJSON(DataFormationHelper.convertFromJSONToHierarchicalDataTable(data))).toEqual(data);
 	});
