9838f862302ed117eece765717380e1ceba07d86
Jatupon Sukkasem <jasukkas@outlook.com>
2021-12-06 10:44:18 +0700

    Implement testing for standard structure of DataFormationHelper and DataManipulationHelper.

--- test/boilerplate/back-end/helpers/DataFormationHelper.test.ts
+++ test/boilerplate/back-end/helpers/DataFormationHelper.test.ts
@@ -20,7 +20,295 @@ describe('DataFormationHelper', () => {
 	});
 	
 	test('Standard Structure', () => {
-		
+		const data = {
+	    "CollectionA": {
+	      "source": 6,
+	      "group": "CollectionA",
+	      "rows": [
+	        {
+	          "keys": {
+	            "keyA": 1,
+	            "keyB": "string 1"
+	          },
+	          "columns": {
+	            "keyC": 2,
+	            "keyD": false
+	          },
+	          "relations": {
+	            "CollectionB": {
+	              "source": 6,
+	              "group": "CollectionB",
+	              "rows": [
+	                {
+	                  "keys": {
+	                    "keyA": 3,
+	                    "keyB": "string 2"
+	                  },
+	                  "columns": {
+	                    "keyC": 4,
+	                    "keyD": true
+	                  },
+	                  "relations": {
+	                    "CollectionC": {
+	                      "source": 6,
+	                      "group": "CollectionC",
+	                      "rows": [
+	                        {
+	                          "keys": {
+	                            "keyA": 5,
+	                            "keyB": "string 3"
+	                          },
+	                          "columns": {
+	                            "keyC": 6,
+	                            "keyD": false
+	                          },
+	                          "relations": {}
+	                        }
+	                      ]
+	                    },
+	                    "CollectionD": {
+	                      "source": 6,
+	                      "group": "CollectionD",
+	                      "rows": [
+	                        {
+	                          "keys": {
+	                            "keyA": 55,
+	                            "keyB": "string 33"
+	                          },
+	                          "columns": {
+	                            "keyC": 66,
+	                            "keyD": true
+	                          },
+	                          "relations": {}
+	                        }
+	                      ]
+	                    }
+	                  }
+	                },
+	                {
+	                  "keys": {
+	                    "keyA": 7,
+	                    "keyB": "string 4"
+	                  },
+	                  "columns": {
+	                    "keyC": 8,
+	                    "keyD": false
+	                  },
+	                  "relations": {
+	                    "CollectionC": {
+	                      "source": 6,
+	                      "group": "CollectionC",
+	                      "rows": [
+	                        {
+	                          "keys": {
+	                            "keyA": 9,
+	                            "keyB": "string 5"
+	                          },
+	                          "columns": {
+	                            "keyC": 10,
+	                            "keyD": true
+	                          },
+	                          "relations": {}
+	                        }
+	                      ]
+	                    }, 
+	                    "CollectionD": {
+	                      "source": 6,
+	                      "group": "CollectionD",
+	                      "rows": [
+	                        {
+	                          "keys": {
+	                            "keyA": 99,
+	                            "keyB": "string 55"
+	                          },
+	                          "columns": {
+	                            "keyC": 100,
+	                            "keyD": false
+	                          },
+	                          "relations": {}
+	                        }
+	                      ]
+	                    }
+	                  }
+	                }
+	              ]
+	            }
+	          }
+	        },
+	        {
+	          "keys": {
+	            "keyA": 11,
+	            "keyB": "string 6"
+	          },
+	          "columns": {
+	            "keyC": 12,
+	            "keyD": true
+	          },
+	          "relations": {
+	            "CollectionB": {
+	              "source": 6,
+	              "group": "CollectionB",
+	              "rows": [
+	                {
+	                  "keys": {
+	                    "keyA": 13,
+	                    "keyB": "string 7"
+	                  },
+	                  "columns": {
+	                    "keyC": 14,
+	                    "keyD": false
+	                  },
+	                  "relations": {
+	                    "CollectionC": {
+	                      "source": 6,
+	                      "group": "CollectionC",
+	                      "rows": [
+	                        {
+	                          "keys": {
+	                            "keyA": 15,
+	                            "keyB": "string 8"
+	                          },
+	                          "columns": {
+	                            "keyC": 16,
+	                            "keyD": true
+	                          },
+	                          "relations": {}
+	                        }
+	                      ]
+	                    }
+	                  }
+	                },
+	                {
+	                  "keys": {
+	                    "keyA": 17,
+	                    "keyB": "string 9"
+	                  },
+	                  "columns": {
+	                    "keyC": 18,
+	                    "keyD": false
+	                  },
+	                  "relations": {
+	                    "CollectionC": {
+	                      "source": 6,
+	                      "group": "CollectionC",
+	                      "rows": [
+	                        {
+	                          "keys": {
+	                            "keyA": 19,
+	                            "keyB": "string 10"
+	                          },
+	                          "columns": {
+	                            "keyC": 20,
+	                            "keyD": true
+	                          },
+	                          "relations": {}
+	                        }
+	                      ]
+	                    }
+	                  }
+	                }
+	              ]
+	            }
+	          }
+	        }
+	      ]
+	    },
+	    "CollectionD": {
+        "source": 6,
+        "group": "CollectionD",
+        "rows": [
+          {
+            "keys": {
+              "keyA": 11,
+              "keyB": "string 11"
+            },
+            "columns": {
+              "keyC": 22,
+              "keyD": false
+            },
+            "relations": {}
+          }
+        ]
+      }
+	  };
+	  
+	  expect(DataFormationHelper.convertFromJSONToHierarchicalDataTable({
+	  	CollectionA: [{
+	  		$keyA: 1,
+	  		$keyB: "string 1",
+	  		keyC: 2,
+	  		keyD: false,
+	  		CollectionB: [{
+	  			$keyA: 3,
+		  		$keyB: "string 2",
+		  		keyC: 4,
+		  		keyD: true,
+		  		CollectionC: [{
+		  			$keyA: 5,
+			  		$keyB: "string 3",
+			  		keyC: 6,
+			  		keyD: false
+		  		}],
+		  		CollectionD: [{
+		  			$keyA: 55,
+			  		$keyB: "string 33",
+			  		keyC: 66,
+			  		keyD: true
+		  		}]
+	  		}, {
+	  			$keyA: 7,
+		  		$keyB: "string 4",
+		  		keyC: 8,
+		  		keyD: false,
+		  		CollectionC: [{
+		  			$keyA: 9,
+			  		$keyB: "string 5",
+			  		keyC: 10,
+			  		keyD: true
+		  		}],
+		  		CollectionD: [{
+		  			$keyA: 99,
+			  		$keyB: "string 55",
+			  		keyC: 100,
+			  		keyD: false
+		  		}]
+	  		}]
+	  	}, {
+	  		$keyA: 11,
+	  		$keyB: "string 6",
+	  		keyC: 12,
+	  		keyD: true,
+	  		CollectionB: [{
+	  			$keyA: 13,
+		  		$keyB: "string 7",
+		  		keyC: 14,
+		  		keyD: false,
+		  		CollectionC: [{
+		  			$keyA: 15,
+			  		$keyB: "string 8",
+			  		keyC: 16,
+			  		keyD: true
+		  		}]
+	  		}, {
+	  			$keyA: 17,
+		  		$keyB: "string 9",
+		  		keyC: 18,
+		  		keyD: false,
+		  		CollectionC: [{
+		  			$keyA: 19,
+			  		$keyB: "string 10",
+			  		keyC: 20,
+			  		keyD: true
+		  		}]
+	  		}]
+	  	}],
+	  	CollectionD: [{
+	  		$keyA: 11,
+	  		$keyB: "string 11",
+	  		keyC: 22,
+	  		keyD: false
+	  	}]
+	  }).rows[0].relations).toEqual(data);
 	});
 	
 	test('Complex Structure', () => {
