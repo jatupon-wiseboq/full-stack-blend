fd6c3dd5007701e7fccfda817e8885f5e7ed4e11
Jatupon Sukkasem <jasukkas@outlook.com>
2021-12-27 21:21:47 +0700

    Fixed: connectors, workers, schedulers' compiling bugs.

---
+++ src/controllers/schedulers/Base.ts
@@ -0,0 +1,24 @@
+// Auto[Generating:V1]--->
+// PLEASE DO NOT MODIFY BECUASE YOUR CHANGES MAY BE LOST.
+
+import {ActionType} from "../helpers/DatabaseHelper";
+import {DataTableSchema} from "../helpers/SchemaHelper";
+
+class Base {
+	constructor() {
+	  this.setup();
+  }
+	
+	protected register() {
+		void(0);
+	}
+	
+	protected setup() {
+		void(0);
+	}
+}
+
+export {Base};
+
+// <--- Auto[Generating:V1]
+// PLEASE DO NOT MODIFY BECUASE YOUR CHANGES MAY BE LOST.
