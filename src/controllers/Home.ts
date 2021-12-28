7909ae2103b05b7c3517397956c9b0665d76eb9c
Jatupon Sukkasem <jasukkas@outlook.com>
2021-12-28 11:03:15 +0700

    [continue] Generate code of connectors, workers, schedulers in prpject manager.

--- src/controllers/Home.ts
+++ src/controllers/Home.ts
@@ -6,7 +6,7 @@ import Component3cb10a6e from "./components/account/settings/_3cb10a6e";
 import Component9e885d49 from "./components/account/authenticate/_9e885d49";
 import Componentb160aa0e from "./components/developer/_b160aa0e";
 import Componentindex from "./components/index";
-
+import Connector02699134 from "./components/connectors/_02699134";
 import Worker0caebca0 from "./components/workers/_0caebca0";
 import Schedulere9acd781 from "./components/shedulers/_e9acd781";
 
@@ -22,7 +22,7 @@ export const _b160aa0e = (req: Request, res: Response) => {
 export const index = (req: Request, res: Response) => {
   new Componentindex(req, res, "home/index");
 }
-
+register('connector', Connector02699134);
 register('worker', Connector0caebca0);
 register('scheduler', Connectore9acd781);
 
