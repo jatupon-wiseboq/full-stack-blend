bd0db48cc49e0454b2b4a2ead5db0fec22ceb4ae
Jatupon Sukkasem <jatupon@softenstorm.com>
2022-11-22 19:34:18 +0700

    Add localization helper.

---
+++ src/controllers/helpers/LocalizationHelper.ts
@@ -0,0 +1,46 @@
+// Auto[Generating:V1]--->
+// PLEASE DO NOT MODIFY BECUASE YOUR CHANGES MAY BE LOST.
+
+import {ProjectConfigurationHelper} from "./ProjectConfigurationHelper";
+
+let cachedData = null;
+let cachedDictionary = null;
+
+const LOCALIZATION_LIST_DELIMITER = '~;;;~';
+const LOCALIZATION_ITEM_DELIMITER = '~:::~';
+const LOCALIZATION_HASH_DELIMITER = '~###~';
+
+const LocalizationHelper = {
+	getLanguageSpecification: (): any => {
+		const data = ProjectConfigurationHelper.getLanguageData();
+		
+		if (cachedData != data) {
+			cachedData = data;
+			cachedDictionary = null;
+		}
+		
+		if (cachedDictionary) return cachedDictionary;
+		if (!cachedData) return {};
+	  
+	  const items = data.split(LOCALIZATION_LIST_DELIMITER);
+	  const dictionary = {};
+	  
+	  for (const item of items) {
+	  	const tokens = item.split(LOCALIZATION_ITEM_DELIMITER);
+	  	dictionary[tokens[0].split(LOCALIZATION_HASH_DELIMITER)[0]] = tokens[1];
+	  }
+	  
+	  cachedDictionary = dictionary;
+	  return dictionary;
+	},
+	localize: (text) => {
+		return LocalizationHelper.getLanguageSpecification[text] || text;
+	}
+};
+
+const loc = LocalizationHelper.localize;
+
+export {loc};
+
+// <--- Auto[Generating:V1]
+// PLEASE DO NOT MODIFY BECUASE YOUR CHANGES MAY BE LOST.
