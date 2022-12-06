c1e18e386c85a3ce7ce59156e76f9959dc32d2fc
Jatupon Sukkasem <jasukkas@outlook.com>
2022-12-06 19:57:22 +0700

    Render custom front-end settings.

--- src/public/js/construction/editor/components/content/ProjectManager.tsx
+++ src/public/js/construction/editor/components/content/ProjectManager.tsx
@@ -542,6 +542,7 @@ ${customFooterExternalStylesheets.join('\n')}
 ${customFooterExternalScripts.join('\n')}
 script(type="text/javascript").
   window.localizedData = ${this.state.extensionValues['customLocalizedStrings'] && ("'" + this.state.extensionValues['customLocalizedStrings'].replace(/'/g, "\\'").replace(/\n/g, "\\n") + "'") || 'null'};
+  window.settings = ${this.state.extensionValues['customFrontEndSettings'] && ("'" + this.state.extensionValues['customFrontEndSettings'].replace(/'/g, "\\'").replace(/\n/g, "\\n") + "'") || 'null'};
 script(type="text/javascript" src="/js/Site.bundle.js?version=${(new Date()).getTime()}")
 `;
             
