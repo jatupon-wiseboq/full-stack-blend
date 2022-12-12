d544ee2fd48ce37ac6be24854fd67240ac6a9df1
Jatupon Sukkasem <jasukkas@outlook.com>
2022-12-12 17:06:40 +0700

    Add all possible test cases for DatabaseHelper

--- test/boilerplate/back-end/helpers/DatabaseHelper.test.ts
+++ test/boilerplate/back-end/helpers/DatabaseHelper.test.ts
@@ -1,7 +1,262 @@
 describe('DatabaseHelper', () => {
-	describe('Group', () => {
+	describe('Utilities', () => {
+		test('distinct', () => {
+			expect(1+2).toEqual(3);
+		});
+	});
+	describe('Satisfy Checker', () => {
 		test('Test', () => {
 			expect(1+2).toEqual(3);
+		});
+	});
+	describe('Input Preparation', () => {
+		test('getRows', () => {
+			expect(1+2).toEqual(3);
+		});
+		test('prepareData', () => {
+			expect(1+2).toEqual(3);
+		});
+		test('ormMap', () => {
+			expect(1+2).toEqual(3);
+		});
+		test('formatKeysAndColumns', () => {
+			expect(1+2).toEqual(3);
+		});
+	});
+	describe('Uniform Operation', () => {
+		describe('Relational', () => {
+			describe('Relational', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Relational x Relational', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Relational x Relational x Relational', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+		});
+		describe('Document', () => {
+			describe('Document', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Document x Document', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Document x Document x Document', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+		});
+		describe('Volatile', () => {
+			describe('Volatile', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Volatile x Volatile', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Volatile x Volatile x Volatile', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+		});
+		describe('Worker', () => {
+			describe('Worker', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Worker x Worker', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Worker x Worker x Worker', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+		});
+	});
+	describe('Across Operation', () => {
+		describe('Test', () => {
+			describe('Relational x Document', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Document x Volatile', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Volatile x Worker', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Relational x Volatile', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Relational x Worker', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Document x Worker', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Relational x Document x Volatile', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Document x Volatile x Worker', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Volatile x Worker x Relational', () => {
+				test('Single', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+		});
+	});
+	describe('Recordset Forwarding', () => {
+		describe('Recursive', () => {
+			describe('Single Document', () => {
+				test('Single Record', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple Records', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Document x Document', () => {
+				test('Single Record', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple Records', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Document x Document x Document', () => {
+				test('Single Record', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple Records', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+		});
+		describe('Recurrent', () => {
+			describe('Single Document', () => {
+				test('Single Record', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple Records', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Document x Document', () => {
+				test('Single Record', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple Records', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
+			describe('Document x Document x Document', () => {
+				test('Single Record', () => {
+					expect(1+2).toEqual(3);
+				});
+				test('Multiple Records', () => {
+					expect(1+2).toEqual(3);
+				});
+			});
 		});
 	});
 });
